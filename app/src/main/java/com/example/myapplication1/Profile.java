2dddc8e1ebf0c3b4da28b63998158ef8613604ef
Rithu <rithupindikuri@hmail.com>
2020-11-06 12:15:19 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapplication1/Profile.java
@@ -0,0 +1,26 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Profile extends AppCompatActivity {
+    FirebaseAuth mFirebaseauth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        mFirebaseauth = FirebaseAuth.getInstance();
+        String Email;
+            FirebaseUser user = mFirebaseauth.getCurrentUser();
+        assert user != null;
+        Email = user.getEmail();
+        TextView mail = findViewById(R.id.textView);
+        mail.setText(Email);
+    }
+}
